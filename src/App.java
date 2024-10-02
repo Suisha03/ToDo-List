0d01d04f230ee972db7b9aa138a79ec7c3d04669
Suisha03 <xxxshota2002xxx@gmail.com>
2024-10-02 14:51:15 +0900

    Tested: how to use Swing

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import javax.swing.JFrame;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        JFrame frame = new JFrame("MyTitle");
+        frame.setBounds(100,100,728,400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
