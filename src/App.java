ab8974060fa6892090ad96b6ca5faeb45a23afce
Suisha03 <xxxshota2002xxx@gmail.com>
2024-11-11 12:22:30 +0900

    Learning:FXActionEvent

--- src/App.java
+++ src/App.java
@@ -2,9 +2,17 @@ import javax.swing.JFrame;
 
 public class App {
     public static void main(String[] args){
+        try{
+            String[] params = {"arg1", "arg2"};
+            TestFX.main(params);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        /*
         JFrame frame = new JFrame("MyTitle");
         frame.setBounds(100,100,728,400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+        */
     }
 }
--- src/TestFX.java
+++ src/TestFX.java
@@ -1,21 +1,41 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-
+ 
 public class TestFX extends Application {
-    public static void main(String[] args) throws Exception {
-        launch();
+    Label label;
+    TextField field;
+    Button button;
+ 
+    public static void main(String[] args) {
+        launch(args);
     }
-
+ 
     @Override
     public void start(Stage stage) throws Exception {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+        label = new Label("This is JavaFX!");
+        field = new TextField();
+        button = new Button("Click");
+        // アクションイベント処理の組み込み
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e) {
+                String msg = "you typed: " + field.getText();
+                label.setText(msg);
+            }
+        });
+        BorderPane pane = new BorderPane();
+        pane.setTop(label);
+        pane.setCenter(field);
+        pane.setBottom(button);
+        Scene scene = new Scene(pane, 320, 120);
         stage.setScene(scene);
         stage.show();
     }
+ 
 }
