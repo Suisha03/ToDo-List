6fc671f5e5394e889f6964c3dd37d7b6405cadfd
Suisha03 <xxxshota2002xxx@gmail.com>
2024-11-19 16:50:23 +0900

    Finished: FXTest Desigining GUI

--- src/TestFX.java
+++ src/TestFX.java
@@ -1,45 +1,27 @@
+import java.io.IOException;
+ 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
  
 public class TestFX extends Application {
-    Label label;
-    ToggleGroup group;
- 
     public static void main(String[] args) {
         launch(args);
     }
  
     @Override
-    public void start(Stage stage) throws Exception {
-        label = new Label("This is JavaFX!");
-        group = new ToggleGroup();
-        RadioButton btn1 = new RadioButton("Male");
-        btn1.setToggleGroup(group);
-        btn1.setSelected(true);
-        RadioButton btn2 = new RadioButton("Female");
-        btn2.setToggleGroup(group);
-        btn1.setOnAction((ActionEvent)->{
-            label.setText("you are Male?");
-        });
-        btn2.setOnAction((ActionEvent)->{
-            label.setText("you are Female?");
-        });
-         
-        BorderPane pane = new BorderPane();
-        pane.setTop(label);
-        FlowPane flow = new FlowPane();
-        flow.getChildren().add(btn1);
-        flow.getChildren().add(btn2);
-        pane.setCenter(flow);
-        Scene scene = new Scene(pane, 320, 120);
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) {
+            BorderPane root;
+            try {
+                root = (BorderPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
+                Scene scene = new Scene(root,200,100);
+                stage.setScene(scene);
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
     }
  
 }
