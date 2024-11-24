ba6bc57cf13f3c5399f9592ddf9cf36e9abc3dd4
Suisha03 <xxxshota2002xxx@gmail.com>
2024-11-24 16:04:14 +0900

    Finished: Adding FX

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,27 @@
+import java.net.URL;
+ 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    @Override
-    public void start(Stage primaryStage) {
-        // CheckBoxExampleのインスタンスを作成
-        CheckBoxFunc checkBoxExample = new CheckBoxExample();
-
-        // シーンを作成し、ステージに設定
-        Scene scene = new Scene(checkBoxExample.getVBox(), 200, 100);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("CheckBox Example");
-        primaryStage.show();
-    }
-
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        URL location = getClass().getResource("MainFX.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(location);
+
+        Pane root = (Pane) fxmlLoader.load();
+
+        Scene scene = new Scene(root, 200, 50);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
     }
 }
