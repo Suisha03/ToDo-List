dcd52b79f2a0b497b349f6d0b77fc3411a9bf91d
Suisha03 <xxxshota2002xxx@gmail.com>
2024-11-29 15:14:38 +0900

    Finished: strikethrough

--- src/FXMLController.java
+++ src/FXMLController.java
@@ -1,17 +1,37 @@
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 
 public class FXMLController {
     @FXML
-    private CheckBox checkBox;
+    private CheckBox taskBox1;
+    @FXML
+    private CheckBox taskBox2;
+    @FXML
+    private HBox container;
 
     @FXML
     public void initialize(){
+        setupCheckBox(taskBox1);
+        setupCheckBox(taskBox2);
+    }
+
+    public void setupCheckBox(CheckBox checkBox){
+        Text text = new Text(checkBox.getText());
+        HBox hbox = new HBox(text);
+        hbox.setSpacing(10); // テキストの左に余白を追加
+        checkBox.setGraphic(hbox);
+        checkBox.setText(""); // CheckBoxのテキストを空にする
         checkBox.selectedProperty().addListener((observable, oldValue, newValue) -> {
             if(newValue){
-                checkBox.setStyle("-fx-strikethrough: true;");
+                text.setStrikethrough(true);
+                text.setFill(Color.rgb(110, 108, 108)); // テキストの色を設定
             } else{
-                checkBox.setStyle("-fx-strikethrough: false;");
+                text.setStrikethrough(false);
+                text.setFill(Color.BLACK); // テキストの色を元に戻す
             }
         });
     }
